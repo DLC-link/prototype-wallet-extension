27697336ce4965d0cf91c551f4c42065e93ba90b
sosaucily <sosaucily@gmail.com>
2022-10-12 14:49:53 +0200

    add comments for blockchainExplorerBaseUrl in config

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,11 @@
 import { regtest } from 'bitcoinjs-lib/src/networks'
 
+// For the value of blockchainExplorerBaseUrl:
+// when running the extension from node as a live compiled project (for faster dev and hot-reloading), use http://localhost:8080/electrs/
+// when running the extension as a pre-compiled chrome-extension, use http://localhost:3004/
 const Config = {
   network: regtest,
-  blockchainExplorerBaseUrl: 'http://localhost:8080/electrs/',
+  blockchainExplorerBaseUrl: 'http://localhost:3004/',
 }
 
 export default Config
