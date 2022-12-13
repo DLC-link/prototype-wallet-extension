d8b0bcee388032a8994ad492590515ce86d05b97
Polybius93 <adam.daniel.puskas@polybius-studios.com>
2022-12-13 09:25:49 +0100

    Backup

--- src/components/organisms/NewAddressDialog/index.tsx
+++ src/components/organisms/NewAddressDialog/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import Button from '@mui/material/Button'
 import { FC } from 'react'
-import { useAddressContext } from '../../../providers/AddressProvider'
 import { useSnackbar } from '../../../providers/Snackbar'
 import { Typography } from '@mui/material'
 import ContentCopyIcon from '@mui/icons-material/ContentCopy'
