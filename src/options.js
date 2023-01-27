dbdd2152c65b656a990ab040d279d4cacad301bb
sosaucily <sosaucily@gmail.com>
2023-01-27 15:31:17 +0700

    add-fetch-button-to-options-page

--- src/options.js
+++ src/options.js
@@ -7,4 +7,11 @@ function save_options() {
   })
 }
 
+function fetch_options() {
+  chrome.storage.sync.get(['counterpartyWalletURL'], function (data) {
+    document.getElementById('currVal').innerHTML = data.counterpartyWalletURL;
+  });
+}
+
 document.getElementById('save').addEventListener('click', save_options)
+document.getElementById('fetch').addEventListener('click', fetch_options)
